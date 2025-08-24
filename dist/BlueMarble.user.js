fedeba8cf413f43aeb2fdc391e2602bee599672d
y52 <github@y52.dev>
2025-08-24 14:53:28 +0900

    修正: 名前空間とリソースURLを更新してリポジトリの一貫性を保つ

--- dist/BlueMarble.user.js
+++ dist/BlueMarble.user.js
@@ -1,23 +1,20 @@
 // ==UserScript==
 // @name         Blue Marble
-// @namespace    https://github.com/SwingTheVine/
+// @namespace    https://github.com/y52en/?SwingTheVine
 // @version      0.85.0
 // @description  A userscript to automate and/or enhance the user experience on Wplace.live. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with Wplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Blue Marble" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by NASA.
 // @author       SwingTheVine
 // @license      MPL-2.0
-// @supportURL   https://discord.gg/tpeBPy46hf
-// @homepageURL  https://bluemarble.camilledaguin.fr/
-// @icon         https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/051271c433a42db968a865b00f81bb979ee7d13f/dist/assets/Favicon.png
-// @updateURL    https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/main/dist/BlueMarble.user.js
-// @downloadURL  https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/main/dist/BlueMarble.user.js
+// @icon         https://raw.githubusercontent.com/y52en/Wplace-BlueMarble/051271c433a42db968a865b00f81bb979ee7d13f/dist/assets/Favicon.png
+// @updateURL    https://raw.githubusercontent.com/y52en/Wplace-BlueMarble/main/dist/BlueMarble.user.js
+// @downloadURL  https://raw.githubusercontent.com/y52en/Wplace-BlueMarble/main/dist/BlueMarble.user.js
 // @match        https://wplace.live/*
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @grant        GM.setValue
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
-// @connect      telemetry.thebluecorner.net
-// @resource     CSS-BM-File https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/051271c433a42db968a865b00f81bb979ee7d13f/dist/BlueMarble.user.css
+// @resource     CSS-BM-File https://raw.githubusercontent.com/y52en/Wplace-BlueMarble/051271c433a42db968a865b00f81bb979ee7d13f/dist/BlueMarble.user.css
 // ==/UserScript==
 
 // Wplace  --> https://wplace.live
